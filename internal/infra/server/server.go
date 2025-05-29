ee314cb5612ca73fa28f195c105a62b50b8a4383
eliezerraj <eliezerraj@gmail.com>
2025-05-29 11:17:02 -0300

    feat:add sighup

--- internal/infra/server/server.go
+++ internal/infra/server/server.go
@@ -146,11 +146,24 @@ func (h HttpServer) StartHttpAppServer(	ctx context.Context,
 			childLogger.Info().Err(err).Msg("canceling http mux server !!!")
 		}
 	}()
-
+	
 	// Get terl SIGNALS
 	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
-	<-ch
+	signal.Notify(ch, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		sig := <-ch
+
+		switch sig {
+		case syscall.SIGHUP:
+			childLogger.Info().Msg("Received SIGHUP: reloading configuration...")
+		case syscall.SIGINT, syscall.SIGTERM:
+			childLogger.Info().Msg("Received SIGINT/SIGTERM termination signal. Exiting")
+			return
+		default:
+			childLogger.Info().Interface("Received signal:", sig).Send()
+		}
+	}
 
 	if err := srv.Shutdown(ctx); err != nil && err != http.ErrServerClosed {
 		childLogger.Error().Err(err).Msg("warning dirty shutdown !!!")
